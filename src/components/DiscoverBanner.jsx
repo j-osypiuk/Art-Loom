79a00870d8a7c9f36446b18bd4686017e1e9c070
j-osypiuk <kuba6535@gmail.com>
2024-10-19 12:09:07 +0200

    Refactor banner btn design

--- src/components/DiscoverBanner.jsx
+++ src/components/DiscoverBanner.jsx
@@ -1,11 +1,9 @@
-import React, {useRef, useState} from "react";
+import {useRef} from "react";
 import {FaArrowDownLong} from "react-icons/fa6";
-import {Colors, getRandomDarkColor} from "../utils/colors.js";
 
 const DiscoverBanner = ({scrollSectionRef}) => {
 
     const btnRef = useRef(null);
-    const [btnColor, setBtnColor] = useState(getRandomDarkColor());
 
     const handleBtnClick = () => {
         window.scrollBy({
@@ -16,32 +14,11 @@ const DiscoverBanner = ({scrollSectionRef}) => {
         // scrollSectionRef.current.scrollIntoView({behavior: "smooth"});
     }
 
-    const changeBtnColor = (event) => {
-        if (event.target === btnRef.current) {
-            let newColor = getRandomDarkColor();
-            while (true) {
-                if (btnColor !== newColor) {
-                    setBtnColor(newColor);
-                    break;
-                } else {
-                    newColor = getRandomDarkColor();
-                }
-            }
-        }
-    }
-
     return (
         <div className="cmp-discover-banner">
             <img src="src/assets/discover-bg.jpg" className="cmp-discover-banner__bg-img" alt="Banner photo"/>
             <h1 className="cmp-discover-banner__heading">Each Piece Has a Tale to Tell.</h1>
-            <button ref={btnRef}
-                    type="submit"
-                    style={{backgroundColor: `var(${btnColor})`}}
-                    className="cmp-discover-banner__btn"
-                    onMouseEnter={changeBtnColor}
-                    onMouseLeave={changeBtnColor}
-                    onClick={handleBtnClick}
-            >
+            <button ref={btnRef} type="submit" className="cmp-discover-banner__btn" onClick={handleBtnClick}>
                 <span>Discover</span>
                 <FaArrowDownLong className="cmp-discover-banner__btn-arrow"/>
             </button>
