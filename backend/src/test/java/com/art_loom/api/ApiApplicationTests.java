17eb83c3e1ee3789b841f72c6eb33759d46afd14
j-osypiuk <kuba6535@gmail.com>
2024-10-24 15:19:12 +0200

    Create backend project

---
+++ backend/src/test/java/com/art_loom/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art_loom.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
